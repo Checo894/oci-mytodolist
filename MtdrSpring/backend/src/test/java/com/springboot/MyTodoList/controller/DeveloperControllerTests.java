44b644c4d8ecf70d30b1c7aa1e328b7c71f9e53a
MoniEscobedo <A01642647@tec.mx>
2025-04-07 10:29:12 -0600

    feat: Create endpoint to register a developer

--- MtdrSpring/backend/src/test/java/com/springboot/MyTodoList/controller/DeveloperControllerTests.java
+++ MtdrSpring/backend/src/test/java/com/springboot/MyTodoList/controller/DeveloperControllerTests.java
@@ -2,19 +2,20 @@ package com.springboot.MyTodoList.controller;
 
 import java.util.List;
 
+import static org.hamcrest.Matchers.hasSize;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.springframework.http.MediaType;
 import com.springboot.MyTodoList.model.Developer;
 import com.springboot.MyTodoList.service.DeveloperService;
 
@@ -43,5 +44,14 @@ public class DeveloperControllerTests {
             .andExpect(jsonPath("$", hasSize(1)))
             .andExpect(jsonPath("$[0].name").value("Emiliano Neaves Ortiz"))
             .andExpect(jsonPath("$[0].role").value("developer"));
+    }
+
+    @Test
+    public void shouldRegisterDeveloperSuccessfully() throws Exception {
+        mockMvc.perform(post("/developers/register")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content("{ \"name\": \"Monica Escobedo Cota\", \"email\": \"a01642647@tec.mx\", \"passwordHash\": \"qwer1234\", \"phoneNumber\": \"+523334614250\", \"role\": \"developer\" }"))
+        .andExpect(status().isOk());
+        
     }
 }
