9a389de2398f3dcde97598a8905a12b7a9716959
DanielaRochaMunoz <A00573664@tec.mx>
2025-04-29 18:30:36 -0600

    Agregar prueba vacía para activar tests en CI

---
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/controller/DeveloperControllerTest.java
@@ -0,0 +1,13 @@
+package com.springboot.MyTodoList.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DeveloperControllerTest {
+
+    @Test
+    public void sampleTest() {
+        assertTrue(true); // Esto siempre va a pasar
+    }
+}
